e396698484d069d5438fa7f08db450d048b3dcae
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-08-02 22:47:50 +0530

    render error

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://linkedln-clone-lac.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://linkedln-clone2.onrender.com'],
   credentials: true
 } 
 ))
